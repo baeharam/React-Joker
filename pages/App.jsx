32206261d6e3254c550371abb778783fcb8e4953
bae haram <hisfedev@gmail.com>
2019-10-10 22:53:15 +0900

    Add trailer to router
    Signed-off-by: bae haram <hisfedev@gmail.com>

--- pages/App.jsx
+++ pages/App.jsx
@@ -5,19 +5,21 @@ import Header from './common/Header';
 import Footer from './common/Footer';
 import About from './about/About';
 import Home from './home/Home';
+import Trailer from './trailer/Trailer';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-      <>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-        </Switch>
-        <Footer />
-      </>
+        <>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/trailer" component={Trailer} />
+          </Switch>
+          <Footer />
+        </>
       </BrowserRouter>
     );
   }
